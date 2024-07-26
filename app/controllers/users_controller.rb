b78a1a1b413f182f41bf4fa1934c077559d1dcea
komalle0booth <komalle0@chicagobooth.edu>
2024-07-26 03:50:25 +0000

    index username and show details fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    matching_users = User.all
-    @users = matching_users.order(:created_at)
+    @users = User.all
+    
+    # matching_users = User.all
+    # @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users"})
+    render({ :template => "user_templates/all_users"})
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "users", :action => "home" })
-  get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
+  get("/", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:username", { :controller => "users", :action => "show" })
   post("/insert_user_record", { :controller => "venues", :action => "create" })
   post("/update_users/:user_id", { :controller => "users", :action => "update" })
 
